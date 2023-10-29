a5628b59ad42e7d4150c6801edd6c0bc6e417b78
sagacity0321 <citronica0321@gmail.com>
2023-10-29 16:42:41 +0900

    가게 리스트 클릭하면 상세 Ui로 이동

--- app/src/main/java/com/example/pizzaorderapp_20231029/MainActivity.kt
+++ app/src/main/java/com/example/pizzaorderapp_20231029/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.pizzaorderapp_20231029
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
@@ -23,5 +24,12 @@ class MainActivity : AppCompatActivity() {
         mAdapter = StoreAdapter(this, R.layout.pizza_store_list_item, mPizzaStoreList)
 
         binding.pizzaStoreListView.adapter = mAdapter
+
+        binding.pizzaStoreListView.setOnItemClickListener { adapterView, view, position, l ->
+            val clickedStore = mPizzaStoreList[position]
+
+            val myIntent = Intent(this, ViewStoreDetail::class.java)
+            startActivity(myIntent)
+        }
     }
 }
